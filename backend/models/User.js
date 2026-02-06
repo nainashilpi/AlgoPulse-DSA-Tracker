9256912cfbd394f31f0a79c85f2122372940b687
_Naina_Shilpii <nainay053@gmail.com>
2026-02-06 15:11:19 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -31,7 +31,7 @@ const userSchema = new mongoose.Schema({
   },
   role: {
     type: String,
-    enum: ['User', 'Admin'],
+    enum: ['User', 'Admin', 'user', 'admin'],
     default: 'User',
   },
   
